50c9989957e7e3366ca5833c3e62b848e7036a71
JFreud <jrm.frdnbrg@gmail.com>
2018-05-05 15:21:33 -0400

    finished

--- gmath.c
+++ gmath.c
@@ -9,20 +9,24 @@
 //lighting functions
 color get_lighting( double *normal, double *view, color alight, double light[2][3], double *areflect, double *dreflect, double *sreflect) {
   color i;
+  normalize(normal);
+  normalize(view);
+  normalize(light[LOCATION]);
   color amb = calculate_ambient(alight, areflect);
   color diff = calculate_diffuse(light, dreflect, normal);
   color spec = calculate_specular(light, sreflect, view, normal);
   i.red = amb.red + diff.red + spec.red;
   i.green = amb.green + diff.green + spec.green;
   i.blue = amb.blue + diff.blue + spec.blue;
+  limit_color(&i);
   return i;
 }
 
 color calculate_ambient(color alight, double *areflect ) {
   color a;
-  a.red = alight.red * *areflect;
-  a.green = alight.green * *areflect;
-  a.blue = alight.blue * *areflect;
+  a.red = alight.red * areflect[RED];
+  a.green = alight.green * areflect[GREEN];
+  a.blue = alight.blue * areflect[BLUE];
   limit_color(&a);
   return a;
 }
@@ -32,18 +36,35 @@ color calculate_diffuse(double light[2][3], double *dreflect, double *normal ) {
   // double x = light[0][0];
   // double y = light[0][1];
   // double z = light[0][2];
-  d.red = light[1][0] * *dreflect * dot_product(normal, light[0]);
-  d.green = light[1][1] * *dreflect * dot_product(normal, light[0]);
-  d.blue = light[1][2] * *dreflect * dot_product(normal, light[0]);;
+  double dotp = dot_product(normal, light[LOCATION]);
+  d.red = light[COLOR][RED] * dreflect[RED] * dotp;
+  d.green = light[COLOR][GREEN] * dreflect[GREEN] * dotp;
+  d.blue = light[COLOR][BLUE] * dreflect[BLUE] * dotp;
   limit_color(&d);
   return d;
 }
 
 color calculate_specular(double light[2][3], double *sreflect, double *view, double *normal ) {
+  // PKs([2(N • L)N - L] • V)^x
   color s;
   s.red = 0;
   s.green = 0;
   s.blue = 0;
+  //2(N • L):
+  double nl = 2 * dot_product(normal, light[LOCATION]);
+  double arr[3];
+  arr[0] = nl * normal[0] - light[LOCATION][0];
+  arr[1] = nl * normal[1] - light[LOCATION][1];
+  arr[2] = nl * normal[2] - light[LOCATION][2];
+  double dotp = dot_product(arr, view);
+  if (dotp <= 0) {
+    return s;
+  }
+  double result = pow(dotp, 16);
+  s.red = light[COLOR][RED] * sreflect[RED] * result;
+  s.green = light[COLOR][GREEN] * sreflect[GREEN] * result;
+  s.blue = light[COLOR][BLUE] * sreflect[BLUE] * result;
+  limit_color(&s);
   return s;
 }
 
@@ -53,6 +74,9 @@ void limit_color( color * c ) {
   c->red = c->red > 255 ? 255 : c->red;
   c->green = c->green > 255 ? 255 : c->green;
   c->blue = c->blue > 255 ? 255 : c->blue;
+  c->red = c->red < 0 ? 0 : c->red;
+  c->green = c->green < 0 ? 0 : c->green;
+  c->blue = c->blue < 0 ? 0 : c->blue;
 }
 
 //vector functions
